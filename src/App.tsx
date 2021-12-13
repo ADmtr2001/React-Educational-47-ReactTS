35d5364f923be433de79de8017ac75fe25ec1beb
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-12-13 20:30:05 +0200

    try props types

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Card, { CardVariant } from "./components/Card";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Card
+        width='200px'
+        height='200px'
+        variant={CardVariant.primary}
+        onClick={(num) => console.log(num)}
+      >
+        <button>Click Me!</button>
+        <div>Hello</div>
+      </Card>
+    </div>
+  );
 };
 
 export default App;
