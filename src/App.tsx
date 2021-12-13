1b55e4189dc9cfb30b06c03848b2877af2c714e5
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-12-13 20:45:07 +0200

    add UserList and UserItem component with typed props. Create separate file with types. add IUser interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 import React from "react";
 import Card, { CardVariant } from "./components/Card";
+import UserList from "./components/UserList";
+import { IUser } from "./types/types";
 
 const App = () => {
+  const users: IUser[] = [
+    {
+      id: 1,
+      name: "Leanne Graham",
+      email: "Sincere@april.biz",
+      address: {
+        street: "Kulas Light",
+        city: "Gwenborough",
+        zipcode: "92998-3874",
+      },
+    },
+    {
+      id: 2,
+      name: "Ervin Howell",
+      email: "Shanna@melissa.tv",
+      address: {
+        street: "Victor Plains",
+        city: "Wisokyburgh",
+        zipcode: "90566-7771",
+      },
+    },
+  ];
   return (
     <div>
       <Card
@@ -13,6 +37,7 @@ const App = () => {
         <button>Click Me!</button>
         <div>Hello</div>
       </Card>
+      <UserList users={users} />
     </div>
   );
 };
