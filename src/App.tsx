9d7927c2cd21e2389c6b9940c2534f97b352526e
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-12-13 21:42:56 +0200

    add generic List component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Card, { CardVariant } from "./components/Card";
+import List from "./components/List";
+import UserItem from "./components/UserItem";
 import UserList from "./components/UserList";
 import { IUser } from "./types/types";
 
@@ -33,7 +35,10 @@ const App = () => {
         <button>Click Me!</button>
         <div>Hello</div>
       </Card>
-      <UserList users={users} />
+      <List
+        items={users}
+        renderItem={(user: IUser) => <UserItem user={user} key={user.id} />}
+      />
     </div>
   );
 };
